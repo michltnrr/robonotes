4a5050b22ab10c8f9f585a8110583a4f2dabbc15
michael <michaelturner@MacBookAir.attlocal.net>
2025-07-22 18:55:47 -0400

    touch ups to summary.js

--- public/summary.js
+++ public/summary.js
@@ -3,6 +3,7 @@ const textarea = document.querySelector(`#generatedContent`)
 const button = document.querySelector(`.generator`)
 
 async function getData() {
+    textarea.textContent = `Summarizing...`
     const summaryFetch = await fetch(`/api/summary?url=${encodeURIComponent(url.value)}`)
     const {summarization} = await summaryFetch.json()
     textarea.textContent = summarization
