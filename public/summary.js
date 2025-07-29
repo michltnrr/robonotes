bfff9dc47759c9412da1d278666707665e4c8266
michael <michaelturner@MacBookAir.attlocal.net>
2025-07-29 11:40:58 -0400

    added copy to clipboard and clear inputs functionality for yt summaries

--- public/summary.js
+++ public/summary.js
@@ -2,12 +2,41 @@ const url = document.querySelector(`.vidLink`)
 const textarea = document.querySelector(`#generatedContent`)
 const button = document.querySelector(`.generator`)
 
+const copyButton = document.querySelector(`#clipboard`)
+const clearButton = document.querySelector(`#clear`)
+
+function copyClipboard() {
+    textarea.select();
+    textarea.setSelectionRange(0, 999999)
+    
+    navigator.clipboard.writeText(textarea.value)
+}
+
+function clearInputs() {
+    url.value = ''
+    textarea.value = ''
+    copyButton.hidden = true
+    clearButton.hidden = true
+}
+
 async function getData() {
-    //make try catch block here and display error in textarea if needed
-    textarea.textContent = `Summarizing...`
-    const summaryFetch = await fetch(`/api/summary?url=${encodeURIComponent(url.value)}`)
-    const {summarization} = await summaryFetch.json()
-    textarea.textContent = summarization
+    try {
+        if(!url.value){
+            alert("Please enter a link from youtube.com")
+        }
+        
+        textarea.textContent = `Summarizing...`
+        const summaryFetch = await fetch(`/api/summary?url=${encodeURIComponent(url.value)}`)
+        const {summarization} = await summaryFetch.json()
+        textarea.textContent = summarization
+        copyButton.hidden = false
+        clearButton.hidden = false
+        
+    } catch (err) {
+        textarea.textContent = `Something went wrong with your request, Please try again.`
+    }
 }
 
 button.addEventListener(`click`, getData)
+copyButton.addEventListener(`click`, copyClipboard)
+clearButton.addEventListener(`click`, clearInputs)
