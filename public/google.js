182233e144bad0a8f37d9e3b8119d44ae5c5cb4d
michael <michaelturner@MacBookAir.attlocal.net>
2025-08-02 18:50:15 -0400

    added basic text styling functionality in google.js

--- public/google.js
+++ public/google.js
@@ -1,5 +1,6 @@
 const {google} = require(`googleapis`)
 require('dotenv').config()
+const fs = require(`fs`)
 
 const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS)
 const auth = new google.auth.GoogleAuth({
@@ -13,8 +14,11 @@ async function writeDocument(mydoucmentId) {
             version: `v1`,
             auth
         })
-
-        const writer = await docs.documents.batchUpdate({
+        const essayFile = fs.readFileSync(`generated-essay.json`).toString()
+        const paperData = JSON.parse(essayFile)
+        const documentText = `${paperData.usersName}\n${paperData.className}\n${paperData.professorName}\n\n\t\t${paperData.title}\n\n\b ${paperData.intro} ${paperData.body} ${paperData.conclusion}`
+        
+        const writter = await docs.documents.batchUpdate({
             documentId: mydoucmentId,
             requestBody: {
                 requests: {
@@ -22,11 +26,39 @@ async function writeDocument(mydoucmentId) {
                         location: {
                             index: 1,
                         },
-                        text: `This paper should now be ready to be written completely using ai, which should be easy, the hard part will be diggign throught the docs for formatting. \nOnce we do that, we're in the green.`
+                        text: documentText,
                     }
                 }
             }
         })
+        
+        //for mla format the date is also needed, need to add that 
+        const formatter = await docs.documents.batchUpdate({
+            documentId: mydoucmentId,
+            requestBody: {
+                requests: {
+                    updateTextStyle: {
+                        range: {
+                            startIndex: 1,
+                            endIndex: 1+ documentText.length,
+                        },
+                        textStyle: {
+                            weightedFontFamily: {
+                                fontFamily: `Tinos`,
+                            },
+                            
+                            fontSize: {
+                                magnitude: 12,
+                                unit: `PT`
+                            },
+                        },
+                        fields: `weightedFontFamily,fontSize` 
+                        
+                    }, 
+                }
+            }
+        })
+
     } catch(err) {
         console.error(err)
     }
