71271f1136eebf84bf666c636ea9ab4d06a217c2
michael <michaelturner@MacBookAir.attlocal.net>
2025-08-01 20:23:31 -0400

    added routes in app.js for ai writer, rewrote ai-writer.js, not working atm

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const hbs = require(`hbs`)
 const path = require(`path`)
 const {getTranscript} = require(`./public/youtube.js`)
 const {main} = require(`./public/openai-app.js`)
+const { default: OpenAI } = require("openai")
 
 const app = express()
 
@@ -48,6 +49,24 @@ app.get(`/assistant`, (req, res) => {
     res.render(`assistant`, {
         name: `Mike Turner`
     })
+})
+
+app.get(`/writer/assistant`, async (req, res) => {
+    
+    if(!req.query.pages || !req.query.title || !req.query.profName || !req.query.usersName || !req.query.className || !req.query.guidelines) {
+            return res.send(`Your request must include the query key-value pairs for the title, professor name, writer name, classname, guidelines, and number of pages`)
+        }
+
+        const client = new OpenAI()
+        const response = await client.responses.create({
+            model: `gpt-4.1`,
+            input: `Your an essay writer, you need to write a college level essay. The guidelines are as follows: ${req.query.guidelines}, it must be ${req.query.pages} pages long
+        the professor's name, writer/users name, className, and essay title must be included in the response, professor name is ${req.query.profName}, classname is ${req.query.className}, users name is${req.query.usersName}, and the title is ${req.query.essayTitle}
+        please return the essay in JSON format where the title, professor name, usersName, and className are properties on the object and so is the essays intro, body, and conclusion, all are props on the JSON object`,
+        })
+    
+        const essayJSON = response.output_text
+        res.send(essayJSON)
 })
 
 app.listen(4000, () => {
--- public/ai-writer.js
+++ public/ai-writer.js
@@ -1,8 +1,8 @@
 // const dotenv = require('dotenv')
 // const OpenAI = require ('openai');
-import dotenv from 'dotenv'
-import OpenAI from 'openai'
-dotenv.config()
+// import dotenv from 'dotenv'
+// import OpenAI from 'openai'
+// dotenv.config()
 
 // const pages = 2
 // const essayTitle = `African American History`
@@ -11,7 +11,7 @@ dotenv.config()
 // const className = `AFS 1100`
 // const guidelines = `Essay must be 2-3 pages long, times new roman, 12 pt font`
 const pages = document.querySelector(`#num-pages`).value
-const essayTitle = document.querySelector(`#essay-title`).value
+const essayTitle = document.querySelector(`#title`).value
 const profName = document.querySelector(`#professor`).value
 const usersName = document.querySelector(`#users-fullname`).value
 const className = document.querySelector(`#classname`).value
@@ -20,19 +20,17 @@ const guidelines = document.querySelector(`#guidelines`).value
 const generateButton = document.querySelector(`#essay-link`)
 
 
-/*I THINK I NEED TO HAVE THE SERVER MAKE THE OPEN AI REQUEST, make a JSON endpoint and use fetch to send the data using the dom values*/
+/*I THINK I NEED TO HAVE THE SERVER MAKE THE OPEN AI REQUEST, make a JSON endpoint and use fetch to send the data using the dom values, in the server file
+the dom values will be replaced as query key val pairs*/
 generateButton.addEventListener(`click`, async () => {
-    const client = new OpenAI()
-    
-    const response = await client.responses.create({
-        model: 'gpt-4.1',
-        input: `Your an essay writer, you need to write a college level essay. The guidelines are as follows: ${guidelines}, it must be ${pages} pages long
-        the professor's name, writer/users name, className, and essay title must be included in the response, professor name is ${profName}, classname is ${className}, users name is${usersName}, and the title is ${essayTitle}
-        please return the essay in JSON format where the pages, title, professor name, usersName, className, and guidliness are properties on the object and so is the essays intro, body, and conclusion, all are props on the JSON object`
-    })
-    
-        console.log(response.output_text)
-    })
+        try{
+            const essayContents = fetch(`/writer/assistant?pages=${encodeURIComponent(pages)}&title=${encodeURIComponent(essayTitle)}&profName=${encodeURIComponent(profName)}&usersName=${encodeURIComponent(usersName)}&className=${encodeURIComponent(className)}&=guidlines=${encodeURIComponent(guidelines)}`)
+            console.log(essayContents)
+            
+        } catch(err) {
+            console.error(err)
+        }
+})
     
     
 //     async function writeEssay() {
