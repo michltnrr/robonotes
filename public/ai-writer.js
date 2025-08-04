0bcb36fab18534a99466641346dc5918a7eaa412
michael <michaelturner@MacBookAir.attlocal.net>
2025-08-03 22:18:50 -0400

    started writing logic for essay date

--- public/ai-writer.js
+++ public/ai-writer.js
@@ -1,4 +1,26 @@
 const generateButton = document.querySelector(`#essay-link`)
+const dateItems = document.querySelector(`.date-items`)
+const mla = document.querySelector(`#mla-format`)
+const apa = document.querySelector(`#apa-format`)
+
+let format, date;
+
+//I NEED TO DECIDE IF I WANT DATE TO BE APART OF THE FETCH BY DEFAULT OR CONDITIONALLY
+function specifyFormat() {
+    if(mla.checked === true) {
+        format = 'MLA'
+        dateItems.hidden = false
+        const creationDate = document.querySelector(`#paper-date`).value
+    } else if (apa.checked === true) {
+        format = 'APA'
+        dateItems.hidden = true
+    }
+
+    return format
+
+}
+mla.addEventListener('change', specifyFormat)
+apa.addEventListener('change', specifyFormat)
 
 async function fetchEssayData(e) {
     e.preventDefault()
@@ -9,6 +31,10 @@ async function fetchEssayData(e) {
     const usersName = document.querySelector(`#users-fullname`).value
     const className = document.querySelector(`#classname`).value
     const guidelines = document.querySelector(`#guidelines`).value
+
+    const essayFormat = specifyFormat()
+    console.log(essayFormat)
+
     
     const url = `/writer/assistant?pages=${encodeURIComponent(pages)}&title=${encodeURIComponent(essayTitle)}&profName=${encodeURIComponent(profName)}&usersName=${encodeURIComponent(usersName)}&className=${encodeURIComponent(className)}&guidelines=${encodeURIComponent(guidelines)}`
     const essayFetch = await fetch(url)
@@ -16,5 +42,4 @@ async function fetchEssayData(e) {
     console.log(essayData) 
     
 }
-
 generateButton.addEventListener(`click`, fetchEssayData)
